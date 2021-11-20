642d486f37117261f029cf278f4dd51f0f779448
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2021-11-20 14:10:15 +1100

    updt: added roles menu and display. added inquirer for adding new role

--- server.js
+++ server.js
@@ -60,6 +60,7 @@ const mainMenu = () => {
     })
 };
 
+//DEPARTMENT
 const viewDept = () => {
   const sql = `SELECT * FROM department;`;
   db.query(sql, (err, res) => {
@@ -77,7 +78,7 @@ const viewDeptMenu = () => {
   inquirer.prompt([
     {
     type: "list",
-    message: "Departments: What would you like to do?",
+    message: "Departments- What would you like to do?",
     name: "choice",
     choices: [
               "a. Add Department", 
@@ -95,7 +96,7 @@ const viewDeptMenu = () => {
             {
               name: "dept_name",
               type: "input",
-              message: "Please enter new Department name:"
+              message: "Add Department- Please enter new Department name:"
             }
             ]).then(function(res) {
               const ans = res.dept_name;
@@ -108,6 +109,97 @@ const viewDeptMenu = () => {
                 viewDept();
               })
             })
+          break;
+  
+          case "b. Update Department":
+            inquirer.prompt([
+              {
+                name: "the_id",
+                type: "input",
+                message: "Update Department- Please select the id of department you wish to update:"
+              }
+              ]).then(function(res) {
+                const ans_id = res.the_id;
+                inquirer.prompt([
+                  {
+                    name: "dept_name",
+                    type: "input",
+                    message: "Update Department- Please enter new department name:"
+                  }
+                  ]).then(function(res) {
+                    const ans_dept_name = res.dept_name;
+                    const updateSql = `UPDATE department SET dept_name = '${ans_dept_name}' WHERE id = ${ans_id};`;
+                    db.query(updateSql, (err,res) => {
+                        if (err) {
+                          console.log(`Something went wrong. \n${err}`);
+                          return;
+                        }
+                        viewDept();
+                      })
+                    })
+                  })
+          break;
+  
+          case "c. Delete Department":
+            inquirer.prompt([
+              {
+                name: "the_id",
+                type: "input",
+                message: "Delete Department- Please select the id of department you wish to delete:"
+              }
+              ]).then(function(res) {
+                const ans = res.the_id;
+                const deleteSql = `DELETE FROM department WHERE id = ${ans};`;
+                db.query(deleteSql, (err,res) => {
+                  if (err) {
+                    console.log(`Something went wrong. \n${err}`);
+                    return;
+                  }
+                  viewDept();
+                })
+              })
+          break;
+
+          case "d. Back to Main Menu":
+              mainMenu();
+          break;
+        }
+      })
+};
+
+//ROLES
+const viewRoles = () => {
+  const sql = `SELECT * FROM roles;`;
+  db.query(sql, (err, res) => {
+    if (err) {
+      console.log(`Something went wrong. \n${err}`);
+      return;
+    }
+    console.log(`\n=========================================`);
+    console.table(res);
+    viewRolesMenu();
+  })
+};
+
+const viewRolesMenu = () => {
+  inquirer.prompt([
+    {
+    type: "list",
+    message: "Roles- What would you like to do?",
+    name: "choice",
+    choices: [
+              "a. Add Roles", 
+              "b. Update Roles",
+              "c. Delete Roles",
+              "d. Back to Main Menu"
+            ]
+    },
+    ])
+    .then(res => {
+      const selection = res.choice;
+      switch (selection) {
+        case "a. Add Roles":
+          viewRolesDeptTable();
           break;
   
           case "b. Update Department":
@@ -133,7 +225,7 @@ const viewDeptMenu = () => {
                           console.log(`Something went wrong. \n${err}`);
                           return;
                         }
-                        viewDept();
+                        viewRoles();
                       })
                     })
                   })
@@ -154,7 +246,7 @@ const viewDeptMenu = () => {
                     console.log(`Something went wrong. \n${err}`);
                     return;
                   }
-                  viewDept();
+                  viewRoles();
                 })
               })
           break;
@@ -166,20 +258,53 @@ const viewDeptMenu = () => {
       })
 };
 
-const viewRoles = () => {
-  const sql = `SELECT * FROM roles;`;
+const viewRolesDeptTable = () => {
+  const sql = `SELECT department.id, department.dept_name, roles.role_title FROM department, roles WHERE department.id = roles.dept_id;`;
   db.query(sql, (err, res) => {
     if (err) {
       console.log(`Something went wrong. \n${err}`);
       return;
     }
-    console.log(`=========================================`);
+    console.log(`\n=========================================`);
     console.table(res);
-    console.log(`=========================================`);
-    mainMenu();
+    addRoles();
   });
 };
 
+const addRoles = () => {
+  inquirer.prompt([
+    {
+      name: "role_title",
+      type: "input",
+      message: "Add Role- Please enter new Role name:"
+    },
+    {
+      name: "role_salary",
+      type: "input",
+      message: "Add Role- Please enter the salary for this role:"
+    },
+    {
+      name: "role_dept",
+      type: "input",
+      message: "Add Role- Please enter the department number for this role:"
+    }
+    ]).then(res => {
+      const role_title = res.role_title;
+      const role_salary = res.role_salary;
+      const role_dept = res.role_dept;
+      console.log(`${role_title} ${role_salary} ${role_dept}`);
+      // const insertsql = `INSERT INTO roles (role_title, salary, dept_id) VALUE ('${ans}');`;
+      // db.query(insertsql, (err,res) => {
+      //   if (err) {
+      //     console.log(`Something went wrong. \n${err}`);
+      //     return;
+      //   }
+      //   viewRoles();
+      // })
+    })
+};
+
+//EMPLOYEES
 const viewEmp = () => {
   const sql = `SELECT * FROM employee;`;
   db.query(sql, (err, res) => {
